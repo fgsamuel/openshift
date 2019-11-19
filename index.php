1e2d140d518133e7d48d580763e3a3cc586e3cf3
fgsamuel <fg.samuel@gmail.com>
2019-11-19 15:26:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php
-  echo "<h1>Openshift Workshop v1.0</h1> ";
-  echo $_SERVER['SERVER_ADDR'];
-  echo $_SERVER['SERVER_NAME'];
+<?php                    
+  echo "<h1>Openshift Workshop v1.0</h1> ";                    
+  $host= gethostname();
+  $ip = gethostbyname($host);                    
+  echo $ip;
 ?>
